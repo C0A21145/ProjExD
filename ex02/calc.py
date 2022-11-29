5872cd935743788a5a7fee73abfc9307a34ff6a6
Akinori <c0a21145c2@edu.teu.ac.jp>
2022-11-29 14:39:48 +0900

    q2

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,4 +6,24 @@ root.title("tk_calculator")
 root.geometry("300x500")
 
 
+def show_btn():
+    buttons = [0 for _ in range(10)]
+    r, c = 0, 0
+    for i in range(10):
+        buttons[i] = tk.Button(root,
+                               text=f"{abs(9-i)}",
+                               width=4, height=2,
+                               font=("", 30))
+
+        buttons[i].grid(row=r, column=c)
+        buttons[i].bind("<1>", button_click)
+
+        c += 1
+        if c % 3 == 0:
+            r += 1
+            c = 0
+
+
+show_btn()
+
 root.mainloop()
