df81e1ffbae44abdd836dcffb5240d503e04d926
Akinori <c0a21145c2@edu.teu.ac.jp>
2022-12-06 15:20:02 +0900

    size add

--- ex03/maze_maker.py
+++ ex03/maze_maker.py
@@ -1,14 +1,16 @@
 import random
 
+
 def make_maze(yoko, tate):
-    XP = [ 0, 1, 0, -1]
+    XP = [0, 1, 0, -1]
     YP = [-1, 0, 1,  0]
 
-    maze_lst = [[1 for i in range(tate)] for j in range(yoko)]  #大きさがtate*yokoの「1」の2次元リスト
-    for maze_yoko in range(1, len(maze_lst)-1): #壁ではない部分を0にする
+    maze_lst = [[1 for i in range(tate)]
+                for j in range(yoko)]  # 大きさがtate*yokoの「1」の2次元リスト
+    for maze_yoko in range(1, len(maze_lst)-1):  # 壁ではない部分を0にする
         for cell in range(1, len(maze_lst[0])-1):
             maze_lst[maze_yoko][cell] = 0
-    for y in range(2, tate-2, 2): #迷路を作る
+    for y in range(2, tate-2, 2):  # 迷路を作る
         for x in range(2, yoko-2, 2):
             maze_lst[x][y] = 1
             if x > 2:
@@ -18,25 +20,31 @@ def make_maze(yoko, tate):
             maze_lst[x+YP[rnd]][y+XP[rnd]] = 1
     return maze_lst
 
+
 def show_maze(canvas, maze_lst):
     color = ["white", "gray"]
     for x in range(len(maze_lst)):
         for y in range(len(maze_lst[x])):
-            canvas.create_rectangle(x*100, y*100, x*100+100, y*100+100, fill=color[maze_lst[x][y]])
+            #canvas.create_rectangle(x*100, y*100, x*100+100, y*100+100, fill=color[maze_lst[x][y]])
+            canvas.create_rectangle(
+                x*50, y*50, x*50+50, y*50+50, fill=color[maze_lst[x][y]])
 
-#2次元リストを渡すとCUIで迷路を表示
+# 2次元リストを渡すとCUIで迷路を表示
+
+
 def print_maze(maze_lst):
-    maze_lst = [list(x) for x in zip(*maze_lst)] #転置
+    maze_lst = [list(x) for x in zip(*maze_lst)]  # 転置
     for i in maze_lst:
         for j in i:
             if j == 1:
                 j = "■"
             else:
                 j = "□"
-            print(j,end="")
+            print(j, end="")
         print()
 
-#maze_makerテスト用
+
+# maze_makerテスト用
 if __name__ == "__main__":
-    maze = make_maze(15,9)
+    maze = make_maze(15, 9)
     print_maze(maze)
