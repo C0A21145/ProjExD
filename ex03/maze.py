c8f0adc18757bf4bb30fc525f501bdf9e77702f3
Akinori <c0a21145c2@edu.teu.ac.jp>
2022-12-06 14:00:26 +0900

    q3

--- ex03/maze.py
+++ ex03/maze.py
@@ -9,4 +9,10 @@ if __name__ == "__main__":
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
 
+    cx = 300
+    cy = 400
+
+    photo = tk.PhotoImage(file="fig/0.png")
+    canvas.create_image(cx, cy, image=photo)
+
     root.mainloop()
