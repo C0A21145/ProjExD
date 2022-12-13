4d0e91b991ba09241b67e4dca8f431fd17a5e8f8
Akinori <c0a21145c2@edu.teu.ac.jp>
2022-12-13 17:33:07 +0900

    盾修正 #11

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -149,7 +149,7 @@ def main():
                 screen = 2
 
             # GameOver判定
-            if tori_rct.colliderect(bomb_rct) or deadline < 0:
+            if tori_rct.colliderect(bomb_rct):
                 if item_status == 1:
                     vx *= -1
                     vy *= -1
@@ -177,7 +177,7 @@ def main():
             scrn_sfc.blit(gameclear_text, gameclear_text_rct)
 
         pg.display.update()
-        clock.tick(2000)
+        clock.tick(10000)
 
 
 # ----------------------------------------------------------------
