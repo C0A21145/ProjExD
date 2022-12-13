cf711e62789b4c47675fe22912528bd44fe42617
Akinori <c0a21145c2@edu.teu.ac.jp>
2022-12-13 13:40:46 +0900

    q2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -5,8 +5,19 @@ import sys
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
+    bg_sfc = pg.image.load("fig/pg_bg.jpg")
+    bg_rct = bg_sfc.get_rect()
+
     clock = pg.time.Clock()
-    clock.tick(0.5)
+
+    while True:
+        scrn_sfc.blit(bg_sfc, bg_rct)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        pg.display.update()
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
